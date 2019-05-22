7afc489aab29cfb8affc074537e49df085183588
Pü$$y_D3$TrØy3R <37734565+J0033@users.noreply.github.com>
2019-05-22 20:54:16 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'say',	description: 'Make the bot say something', args: true,
 
 	execute(message, args) {
-  if(message.member.permissions.has("manage_guild",
+  if(message.member.permissions.has("MANAGE_GUILD",
 true)) {
     message.delete(10);
     message.channel.send(args.join(" "));
