4fdd6610c47629860d1dc7a2a771caf2fe32ccd6
Pü$$y_D3$TrØy3R <37734565+J0033@users.noreply.github.com>
2019-05-23 13:07:45 +0300

    Update qotd.js

--- commands/qotd.js
+++ commands/qotd.js
@@ -1,12 +1,12 @@
 module.exports = {
 
-	name: 'qotd',
+	name: 'qotd',	cooldown: 86400000,
 
 	description: 'Makes the QOTD role mentionable for 7s and then unmentionable',
 
-	execute(message, args) {
+	execute(message, args) {       
 
-        if(message.member.roles.has("570532388913020929")){
+if(message.member.roles.has("570532388913020929")){
 
 message.guild.roles.find(r => r.name == "QOTD").setMentionable(true, 'QOTD Mention').then(() => {
 
@@ -22,10 +22,12 @@ message.channel.send(`QOTD Role is now unmentionable. <@${message.author.id}>`);
 
 });
 
- }, 8500);
+ }, 7500);
 
 });
+
 }
+
 	},
 
 };
