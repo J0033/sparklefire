f39c4d274d82c8c5aa47d04b6bad399c88267a55
Pü$$y_D3$TrØy3R <37734565+J0033@users.noreply.github.com>
2019-05-23 12:53:29 +0300

    Create qotd.js

---
+++ commands/qotd.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+	name: 'qotd',
+
+	description: 'Makes the QOTD role mentionable for 7s and then unmentionable',
+
+	execute(message, args) {
+
+        if(message.member.roles.has("570532388913020929")){
+
+message.guild.roles.find(r => r.name == "QOTD").setMentionable(true, 'QOTD Mention').then(() => {
+
+            message.channel.send(`QOTD Role is now mentionable for 7 seconds. <@${message.author.id>`);
+
+}).then(() => {
+
+setTimeout(() => {
+
+message.guild.roles.find(r => r.name == "QOTD").setMentionable(flase, 'QOTD Mention Timeout').then(() => {
+
+message.channel.send(`QOTD Role is now unmentionable. <@${message.author.id>`);
+
+});
+
+ }, 8500);
+
+});
+
+	},
+
+};
